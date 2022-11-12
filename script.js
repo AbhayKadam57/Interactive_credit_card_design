05b214b4e1cd8d9d09d9044178cee5a9640d31dc
abhay <kadamabhay40@gmail.com>
2022-11-12 16:00:23 +0530

    Validation for input added

--- script.js
+++ script.js
@@ -1,206 +1,151 @@
-const Name = document.getElementById("name")
+const Name = document.getElementById("name");
 
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 
-const NameError = document.querySelector(".error-name")
+const NameError = document.querySelector(".error-name");
 
-const CardNumber = document.getElementById("number")
+const CardNumber = document.getElementById("number");
 
-const NumberError = document.querySelector(".error-number")
-const DateError = document.querySelector(".error-date")
-const YearError = document.querySelector(".error-date_1")
-const CVVError = document.querySelector(".error-cvv")
+const NumberError = document.querySelector(".error-number");
+const DateError = document.querySelector(".error-date");
+const YearError = document.querySelector(".error-date_1");
+const CVVError = document.querySelector(".error-cvv");
 
+const month = document.getElementById("month");
 
-const month = document.getElementById("month")
+const year = document.getElementById("year");
 
-const year = document.getElementById("year")
+const cvv = document.getElementById("cvv-number");
 
-const cvv = document.getElementById("cvv-number")
+const displayNumber = document.getElementById("card-number");
 
+const displayName = document.getElementById("displayName");
+const displayMonth = document.getElementById("displayMonth");
+const displayYear = document.getElementById("displayYear");
+const displayCvv = document.getElementById("displayCvv");
 
-const displayNumber = document.getElementById("card-number")
+const complete = document.querySelector(".complete");
 
+const type = document.querySelector(".type");
 
-const displayName = document.getElementById("displayName")
-const displayMonth = document.getElementById("displayMonth")
-const displayYear = document.getElementById("displayYear")
-const displayCvv = document.getElementById("displayCvv")
+const Continue = document.getElementById("continue");
 
+const NumberDatatype = document.querySelector(".numberError");
+const CvvNumberDatatype = document.querySelector(".error-cvv-datatype");
 
-const complete = document.querySelector(".complete")
+CardNumber.addEventListener("keyup", (e) => {
+  if (
+    e.target.value.length == 4 ||
+    e.target.value.length == 9 ||
+    e.target.value.length == 14
+  ) {
+    e.target.value += " ";
+  }
+});
 
-const type = document.querySelector(".type")
+CardNumber.addEventListener("click", () => {
+  CardNumber.value = "";
+});
 
-const Continue = document.getElementById("continue")
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-CardNumber.addEventListener("keyup",(e)=>{
+  if (!Name.value) {
+    NameError.style.display = "block";
+    Name.style.border = "2px solid hsl(0, 100%, 66%)";
+  } else {
+    NameError.style.display = "none";
+    Name.style.border = "2px solid hsl(270, 3%, 87%)";
+  }
 
+  const CardNumbers = [...CardNumber.value];
 
-     
-    
-    
-    
-    if(e.target.value.length==4 || e.target.value.length==9 || e.target.value.length==14){
-        
-    
-        e.target.value+=" "
+  let Value = [];
 
-    } 
+  let space = [];
 
-   
-}
-)
-
-CardNumber.addEventListener("click",()=>{
-
-
-    CardNumber.value=""
-
-})
-
-
-
-
-form.addEventListener('submit',(e)=>{
-
-    
-
-    e.preventDefault()
-
-
-    if(!Name.value){
-
-        NameError.style.display="block"
-        Name.style.border="2px solid hsl(0, 100%, 66%)"
-    }else{
-
-        NameError.style.display="none"
-        Name.style.border="2px solid hsl(270, 3%, 87%)"
+  CardNumbers.map((item, index) => {
+    if (item == " ") {
+      space.push(item);
+    } else {
+      Value.push(parseInt(item));
     }
+  });
 
-    const CardNumbers = [...CardNumber.value]    
+  console.log(Value);
 
-    let Value =[]
+  let index = Value.findIndex((item) => isNaN(item));
 
-    let space =[]
+  if (index >= 0 || !CardNumber.value) {
+    NumberError.style.display = "block";
+    CardNumber.style.border = "2px solid hsl(0, 100%, 66%)";
+  } else {
+    NumberError.style.display = "none";
+    CardNumber.style.border = "2px solid hsl(270, 3%, 87%)";
+  }
 
-    CardNumbers.map((item,index)=>{
+  if (!month.value) {
+    month.style.border = "2px solid hsl(0, 100%, 66%)";
+    DateError.style.display = "block";
+  } else {
+    month.style.border = "2px solid hsl(270, 3%, 87%)";
+    DateError.style.display = "block";
+  }
 
-    
+  if (!year.value) {
+    DateError.style.display = "block";
+    year.style.border = "2px solid hsl(0, 100%, 66%)";
+  } else {
+    year.style.border = "2px solid hsl(270, 3%, 87%)";
+    DateError.style.display = "none";
+  }
 
-      if(item==" "){
+  if (!cvv.value) {
+    CVVError.style.display = "block";
+    cvv.style.border = "2px solid hsl(0, 100%, 66%)";
+  } else {
+    CVVError.style.display = "none";
+    cvv.style.border = "2px solid hsl(270, 3%, 87%)";
+  }
 
-        space.push(item)
-      }else{
+  if (
+    index < 0 &&
+    CardNumber.value &&
+    month.value &&
+    year.value &&
+    cvv.value &&
+    Name.value
+  ) {
+    type.style.display = "none";
 
-        Value.push(parseInt(item))
-        
-      }
+    complete.style.display = "flex";
+  }
 
-    
-    })
+  displayNumber.innerText = !CardNumber.value
+    ? "0000 0000 0000 0000"
+    : CardNumber.value;
 
-    console.log(Value)
+  displayName.innerText = !Name.value ? "JANE APPLESEED" : Name.value;
 
+  displayMonth.innerText = !month.value ? "00" : month.value;
 
+  displayYear.innerText = !year.value ? "00" : year.value;
 
+  displayCvv.innerText = !cvv.value ? "000" : cvv.value;
+});
 
-    let index = Value.findIndex(item=>isNaN(item))
+Continue.addEventListener("click", () => {
+  Name.value = "";
 
-    if(index>=0 || !CardNumber.value){
+  CardNumber.value = "";
 
-        NumberError.style.display="block"
-        CardNumber.style.border="2px solid hsl(0, 100%, 66%)"
-    }else{
-        NumberError.style.display="none"
-        CardNumber.style.border="2px solid hsl(270, 3%, 87%)"
-    }
-    
-    
-    if(!month.value){
-        
-        month.style.border="2px solid hsl(0, 100%, 66%)"
-        DateError.style.display="block"
-        
-    }else {
-        month.style.border="2px solid hsl(270, 3%, 87%)"
-        DateError.style.display="block"
+  month.value = "";
 
+  year.value = "";
 
-    }
-    
-    
-    
-    if(!year.value){
+  cvv.value = "";
 
-        DateError.style.display="block"
-        year.style.border="2px solid hsl(0, 100%, 66%)"
-    }else{
+  type.style.display = "flex";
 
-        year.style.border="2px solid hsl(270, 3%, 87%)"
-        DateError.style.display="none"
-    }
-    
-    
-    
-    if(!cvv.value){
-        
-        CVVError.style.display="block"
-        cvv.style.border="2px solid hsl(0, 100%, 66%)"
-    }else{
-
-        CVVError.style.display="none"      
-        cvv.style.border="2px solid hsl(270, 3%, 87%)"
-    
-        
-    }
-
-    
-
-    if(index<0 && CardNumber.value && month.value && year.value && cvv.value && Name.value){
-
-
-        type.style.display="none"
-
-        complete.style.display="flex"
-    }
-
-
-    displayNumber.innerText = !CardNumber.value ? "0000 0000 0000 0000" : CardNumber.value
-
-    displayName.innerText = !Name.value ? "JANE APPLESEED" : Name.value
-
-    displayMonth.innerText = !month.value ? "00" : month.value
-
-
-    displayYear.innerText = !year.value ? "00" : year.value
-
-    displayCvv.innerText = !cvv.value ? "000" : cvv.value
-    
-  
-   
-
-
-})
-
-
-Continue.addEventListener("click",()=>{
-
-    Name.value=""
-
-    CardNumber.value=""
-
-    month.value=""
-
-    year.value=""
-
-    cvv.value=""
-
-    type.style.display="flex"
-
-    complete.style.display="none"
-    
-    
-
-})
+  complete.style.display = "none";
+});
